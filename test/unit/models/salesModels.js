c6201d82b78f0bfda88656c0373b1d2431e55702
Fernanda9421 <fer.candrade21@gmail.com>
2022-04-05 07:33:39 -0300

    testa camada Model - isValidProductId e createSale

--- test/unit/models/salesModels.js
+++ test/unit/models/salesModels.js
@@ -138,3 +138,87 @@ describe('getSaleById', () => {
     });
   });
 });
+
+describe('isValidProductId', () => {
+  describe('Dado que existem produtos no Banco de Dados', () => {
+    const payload = [ 1, 2, 3 ];
+
+    before(() => {
+      const result = [payload];
+      sinon.stub(connection, 'execute').resolves(result);
+    });
+    after(() => {
+      connection.execute.restore();
+    });
+
+    describe('e são um total de 3 produtos', () => {
+      it('retorna um array', async () => {
+        const response = await SalesModel.isValidProductId();
+        expect(response).to.be.an('array');
+      });
+
+      it('o array possui 3 posições', async () => {
+        const response = await SalesModel.isValidProductId();
+        expect(response).to.be.length(3);
+      });
+    });
+  });
+});
+
+describe('createSale', () => {
+  describe('Dado que a venda não existe no Banco de Dados', () => {
+    const payloadArray = [
+      {
+        "productId": 1,
+        "quantity": 3
+      }
+    ];
+
+    const payload = {
+      id: 4,
+      itemsSold: [ { productId: 1, quantity: 3 } ]
+    };
+
+    before(() => {
+      const result = [[payload]];
+      sinon.stub(connection, 'execute').resolves(result);
+    });
+    after(() => {
+      connection.execute.restore();
+    });
+
+    describe('quando é inserido com sucesso', () => {
+      it('retorna um objeto', async () => {
+        const response = await SalesModel.createSale(payloadArray);
+        expect(response).to.be.an('object');
+      });
+
+      it('o objeto possui as propriedades "id" e "itemsSold"', async () => {
+        const response = await SalesModel.createSale(payloadArray);
+        expect(response).to.have.keys('id', 'itemsSold');
+      });
+    });
+
+  // describe('Dado que o produto já existe no Banco de Dados', () => {
+  //   const payload = {
+  //     name: '',
+  //     quantity: 0,
+  //   };
+
+  //   before(() => {
+  //     const execute = [[payload]];
+  //     sinon.stub(connection, 'execute').resolves(execute);
+  //   });
+
+  //   after(() => {
+  //     connection.execute.restore();
+  //   });
+
+  //   describe('quando não é inserido com sucesso', () => {
+  //     it('retorna null', async () => {
+  //       const response = await ProductsModel.createProduct(payload);
+  //       expect(response).to.be.equal(null);
+  //     });
+  //   });
+  });
+});
