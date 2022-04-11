d5913ee3558a3889214ba91939f55785d7e1ba9a
Fernanda9421 <fer.candrade21@gmail.com>
2022-04-11 10:59:09 -0300

    test: salesControllers

--- test/unit/controllers/salesControllers.js
+++ test/unit/controllers/salesControllers.js
@@ -121,3 +121,197 @@ describe('getProductById', () => {
     });
   });
 });
+
+describe('createSale', () => {
+  describe('Dado que o payload é válido', () => {
+    const response = {};
+    const request = {};
+
+    before(() => {
+      request.body =   [
+        {
+          productId: 1,
+          quantity: 3
+        }
+      ];
+
+      const payload = {
+        id: 1,
+        itemsSold: [
+          {
+            productId: 1,
+            quantity: 3
+          }
+        ]
+      };
+
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(SalesService, 'createSale').resolves(payload);
+    });
+
+    after(() => {
+      SalesService.createSale.restore();
+    });
+
+    it('retorna status 201', async () => {
+      await SalesController.createSale(request, response);
+      expect(response.status.calledWith(201)).to.be.equal(true);
+    });
+
+    it('retorna um objeto com o produto', async () => {
+      await SalesController.createSale(request, response);
+      expect(response.json.calledWith(sinon.match.object)).to.be.equal(true);
+    });
+  });
+
+  describe('Dado que o payload é inválido', () => {
+    const response = {};
+    const request = {};
+    const next = sinon.spy();
+
+    before(() => {
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns(response);
+
+      sinon.stub(SalesService, 'createSale').throwsException();
+    });
+    after(() => {
+      SalesService.createSale.restore();
+    });
+
+    it('chama a função next', async () => {
+      await SalesController.createSale(request, response, next);
+      expect(next.calledOnce).to.be.equal(true);
+    });
+  });
+});
+
+describe('updateSale', () => {
+  describe('Dado que o payload é válido', () => {
+    const response = {};
+    const request = {};
+
+    before(() => {
+      request.body =   [
+        {
+          productId: 1,
+          quantity: 2
+        },
+        {
+          productId: 2,
+          quantity: 5
+        }
+      ];
+
+      request.params = { id: 1 };
+
+      const payload = {
+        id: 1,
+        itemsSold: [
+          {
+            productId: 1,
+            quantity: 2
+          },
+          {
+            productId: 2,
+            quantity: 5
+          }
+        ]
+      };
+
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(SalesService, 'updateSale').resolves(payload);
+    });
+
+    after(() => {
+      SalesService.updateSale.restore();
+    });
+
+    it('retorna status 200', async () => {
+      await SalesController.updateSale(request, response);
+
+      expect(response.status.calledWith(200)).to.be.equal(true);
+    });
+
+    it('retorna um objeto com o produto', async () => {
+      await SalesController.updateSale(request, response);
+      expect(response.json.calledWith(sinon.match.object)).to.be.equal(true);
+    });
+  });
+
+  describe('Dado que o payload é inválido', () => {
+    const response = {};
+    const request = {};
+    const next = sinon.spy();
+
+    before(() => {
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns(response);
+
+      sinon.stub(SalesService, 'updateSale').throwsException();
+    });
+    after(() => {
+      SalesService.updateSale.restore();
+    });
+
+    it('chama a função next', async () => {
+      await SalesController.updateSale(request, response, next);
+      expect(next.calledOnce).to.be.equal(true);
+    });
+  });
+});
+
+describe('deleteSale', () => {
+  describe('Dado que o id é válido', () => {
+    const response = {};
+    const request = {};
+
+    before(() => {
+      request.params = { id: 1 }
+
+      const payload = 1;
+
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns(response);
+      response.end = sinon.stub().returns();
+
+      sinon.stub(SalesService, 'deleteSale').resolves(payload);
+    });
+
+    after(() => {
+      SalesService.deleteSale.restore();
+    });
+
+    it('retorna status 204', async () => {
+      await SalesController.deleteSale(request, response);
+
+      expect(response.status.calledWith(204)).to.be.equal(true);
+    });
+  });
+
+  describe('Dado que o id é inválido', () => {
+    const response = {};
+    const request = {};
+    const next = sinon.spy();
+
+    before(() => {
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns(response)
+      response.end = sinon.stub().returns();
+
+      sinon.stub(SalesService, 'deleteSale').throwsException();
+    });
+    after(() => {
+      SalesService.deleteSale.restore();
+    });
+
+    it('chama a função next', async () => {
+      await SalesController.deleteSale(request, response, next);
+      expect(next.calledOnce).to.be.equal(true);
+    });
+  });
+});
